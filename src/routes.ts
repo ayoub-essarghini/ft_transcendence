01456a5f358e6d5e229713662fba66f14ec771e5
aes-sarg <aes-sarg@c4r9p3.1337.ma>
2025-03-21 23:37:24 +0000

    set main empty

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from './utils/router.js';
-// import { Counter } from './components/Counter.js';
-import { App } from './components/App.js';
-
-
-export const routes: Route[] = [
-  { path: "/", component: () => App() },
-//   { path: "/*", component: NotFound },
-];
